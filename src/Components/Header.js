a08afedb32bf0512055694e7d370a1ff73226a41
Mayank-Pandey-Ji <pandeymayank1099@gmail.com>
2024-07-13 13:45:59 +0530

    a small change

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className=' border w-full border-stone-300 shadow-md pb-3 fixed bg-white'>
       <header className='text-center'>
-        <h1 className=' uppercase text-[32px] font-bold  pt-2' >codehelp blogs</h1>
+        <h1 className=' uppercase text-[32px] font-bold  pt-2' >blogs</h1>
       </header>
     </div>
   )
